68c5cddb9aaa9eecc5cf70795b61938185783f8a
Nick Kroes <njkroes@gmail.com>
2023-01-05 14:34:18 -0600

    Updating to pull file path from config

--- python/project1/charliehealth.py
+++ python/project1/charliehealth.py
@@ -6,7 +6,8 @@ import configparser
 config = configparser.ConfigParser()
 config.read('python\project1\config.ini')
 
-file_name = "C:\Temp\outcomes_data.xlsx"
+# Get file path
+file_name = config['files']['file_path']
 columns= ['ParticipantID', 'PreHLCRefYN', 'POSTHLCRefYN', 'NewPreHLCAdmYN', 'POST_HLCAdmYN', 'PreER', 'POST_ER', 'DischargeMonth', 'DischargeCode', 'IntakeMonth', 'Age', 'AgeGroup', 'Gender', 'Transgender', 'SexOrient', 'SexOrientBinary', 'Therapist', 'IntakeDate', 'DateDischarge', 'SessionsAttend', 'WeeksAttend', 'SessionsAttend2', 'PrePHQScore', 'NewPreABASI_Think', 'NewPreABASI_Solution', 'NewPostABASI_Think', 'NewPostABASI_Solution', 'PreERWhy', 'POST_ERWhy', 'PreSelfHarmBinary', 'PostSelfHarmBinary', 'PreASQ1Wished', 'PreASQ2FamBetter', 'PreASQ3ThoughtsWk', 'PreASQ5ThoughtsNow', 'PostASQ1Wished', 'PostASQ2FamBetter', 'PostASQ3ThoughtsWk', 'PostASQ5ThoughtsNow', 'HLCAdm_PreCH', 'PreERNum', 'PrePHQ1', 'PrePHQ2', 'PrePHQ3', 'PrePHQ4', 'PrePHQ5', 'PrePHQ6', 'PrePHQ7', 'PrePHQ8', 'PrePHQ9', 'PostPHQ1', 'PostPHQ2', 'PostPHQ3', 'PostPHQ4', 'PostPHQ5', 'PostPHQ6', 'PostPHQ7', 'PostPHQ8', 'PostPHQ9', 'PreCRAFFTQ1DaysDrink', 'PreCRAFFTQ2DaysMarij', 'PreCRAFFTQ3DaysOther', 'PreCRAFFTQ4DaysRelax', 'PreCRAFFTQ5DaysAlone', 'PreCRAFFTQ6DaysForget', 'PreCRAFFTQ7DaysTrouble', 'PreCRAFFTQ8ToldStop', 'PreCRAFFTQ9Car', 'PostCRAFFTQ1DaysDrink', 'PostCRAFFTQ2DaysMarij', 'PostCRAFFTQ3DaysOther', 'PostCRAFFTQ4DaysRelax', 'PostCRAFFTQ5DaysAlone', 'PostCRAFFTQ6DaysForget', 'PostCRAFFTQ7DaysTrouble', 'PostCRAFFTQ8ToldStop', 'PostCRAFFTQ9Car']
 
 # read in file and sheet to pandas datafame
@@ -15,7 +16,6 @@ df = pd.DataFrame(dfs, columns)
 
 # convert columns to upper case
 df.columns=map(str.upper, df.columns)
-
 
 # get Snowflake connection parameters from config file
 account_identifier = config['snowflake']['account_identifier']      
